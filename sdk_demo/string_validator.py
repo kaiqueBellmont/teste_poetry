1906b066e939d5c675b28676b1b6a32fbb32cbfd
KaiqueBellmont <kaiquebellmont@gmail.com>
2022-11-29 22:28:16 -0300

    outro teste

--- sdk_demo/string_validator.py
+++ sdk_demo/string_validator.py
@@ -1,9 +1,13 @@
 class SimpleStringValidator:
-    def simple_string_validator(self, value):
+    def simple_comparation(self, value1, value2):
         try:
-            if not isinstance(value, str):
-                raise f'O valor {value} não pertence a classe str'
-        except:
-            if isinstance(value, str):
+            if value1 > value2:
                 return True
-
+            return False
+        except TypeError:
+            raise ValueError("Erro de comparação entre tipos")
+        finally:
+            return False
+    @staticmethod
+    def ryan():
+        print("Ryan de outro repositorio")
